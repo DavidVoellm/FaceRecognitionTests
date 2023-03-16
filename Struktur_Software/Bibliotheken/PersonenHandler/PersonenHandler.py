3c7873896f73cb92f6de18e573331486a2cf5752
David Völlm <d-voellm@web.de>
2023-03-16 13:26:06 +0100

    removed debug print

--- Struktur_Software/Bibliotheken/PersonenHandler/PersonenHandler.py
+++ Struktur_Software/Bibliotheken/PersonenHandler/PersonenHandler.py
@@ -7,7 +7,6 @@ class PersonenHandler:
         pass
     def find(self, dir): 
         for file in os.listdir(dir):
-            print(dir) # Debug
             if file.endswith(".jpg"):
                 name = file[:-4]
                 self.personen.append(Person(name, dir+"/"+file))
