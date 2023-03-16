d0d95eb172e5423ae650cb2320be4e111f354671
David Völlm <d-voellm@web.de>
2023-03-16 16:39:43 +0100

    autofocus doesn't exist;)

--- Struktur_Software/Bibliotheken/Camera/Camera.py
+++ Struktur_Software/Bibliotheken/Camera/Camera.py
@@ -4,7 +4,6 @@ import numpy
 class Camera:
     def __init__(self):
         self.cam = cv2.VideoCapture(0)
-        self.cam.set(cv2.CAP_PROP_AUTOFOCUS, 1)
     def __del__(self):
         self.cam.release()
     def get_frame(self): 
