5a86bfd9892678c3be30aeeb99fa4c774ef69022
David Völlm <d-voellm@web.de>
2023-03-16 18:12:01 +0100

    added GPIO mode

--- Struktur_Software/main.py
+++ Struktur_Software/main.py
@@ -1,6 +1,8 @@
 from handler import Handler
 from Bibliotheken import *
 
+GPIO.setmode(GPIO.BCM)
+
 handler = Handler(Buzzer(pin=20), LED(rot=22, gruen=23,blau=24), Servo(pin=17), Camera(0), PersonenHandler(),Gesichtserkennung())
 handler.begin()
 print(handler.PersonenInBild())
