5920fb73e230a9c3c90e979a4e5ab2fd205b2a96
David Völlm <d-voellm@web.de>
2023-03-16 18:02:55 +0100

    finished Buzzer lib and created example use

--- Struktur_Software/Bibliotheken/Buzzer/Buzzer.py
+++ Struktur_Software/Bibliotheken/Buzzer/Buzzer.py
@@ -1,16 +1,19 @@
-class Buzzer:
-    pass
-import sys
 import RPi.GPIO as GPIO
-import time
 
-triggerPIN = 14
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(triggerPIN,GPIO.OUT)
-
-buzzer = GPIO.PWM(triggerPIN, 1000) # Set frequency to 1 Khz
-buzzer.start(10) # Set dutycycle to 10
-
-time.sleep(1)
-GPIO.cleanup()
-sys.exit()
+class Buzzer:
+    def __init__(self, pin=20, defaultFrequency=1000):
+        self.pin = pin
+        GPIO.setup(pin,GPIO.OUT)
+        self.defaultFrequency = defaultFrequency
+        self.buzzer = GPIO.PWM(pin, defaultFrequency)
+        self.buzzer.start(0)
+    def setFrequency(self,frequency):
+        if self.defaultFrequency != frequency:
+            self.defaultFrequency = frequency
+            self.buzzer.ChangeFrequency(frequency)
+    def off(self):
+        self.buzzer.ChangeDutyCycle(0)
+    def on(self):
+        self.buzzer.ChangeDutyCycle(1)
+    def stop(self):
+        self.buzzer.stop()
---
+++ Struktur_Software/Bibliotheken/Buzzer/Buzzerbeispiel.py
@@ -0,0 +1,14 @@
+import sys
+import time
+import RPi.GPIO as GPIO
+from Buzzer import Buzzer
+
+GPIO.setmode(GPIO.BCM)
+buzzer = Buzzer(14)
+buzzer.on()
+
+time.sleep(1)
+buzzer.setFrequency(300)
+time.sleep(1)
+GPIO.cleanup()
+sys.exit()
