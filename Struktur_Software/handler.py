ec7e025eed844fdc1b94542518539278fe4b6ee7
David Voellm <david@voellm-online.de>
2023-03-30 11:08:36 +0200

    Alle Sensoren und akteure ansteuern

--- Struktur_Software/handler.py
+++ Struktur_Software/handler.py
@@ -1,4 +1,5 @@
 from Bibliotheken import *
+import time
 class Handler:
     def __init__(self, Buzzer:Buzzer, LED:LED, Servo, Camera, PersonenHandler, Erkennung, Knopf, oeffneFunc=None, schliessFunc=None):
         self.Buzzer:Buzzer = Buzzer
@@ -18,11 +19,25 @@ class Handler:
         self.Erkennung.set_personen(self.PersonenHandler.get_personen())
     def loop(self):
         if self.Knopf.istGedrueckt():
-            personen = personen_in_bild()
+            print("Knopf gedrückt")
+            personen =self. personen_in_bild()
+            print(personen)
             if self.PersonenHandler.sind_personen_berechtigt(personen):
-                oeffneTuer()
+                self.oeffne_tuer()
+                print("Tür öffnen")
+            else:
+                #in echt nicht tür schliessen bei falschem Gesicht
+                self.schliesse_tuer()
+                self.Buzzer.on()
+                time.sleep(0.5)
+                self.Buzzer.off()
+                
     def oeffne_tuer(self):
-        pass
+        self.Servo.setAngle(180)
+        self.LED.set([0,1,0])
+    def schliesse_tuer(self):
+        self.Servo.setAngle(0)
+        self.LED.set([1,0,0])
     def personen_in_bild(self, bild=None) -> list[str]:
         bild = self.Camera.get_frame() if bild is None else bild
         return self.Erkennung.get_face_names(bild)
--- Struktur_Software/main.py
+++ Struktur_Software/main.py
@@ -5,8 +5,12 @@ GPIO.setmode(GPIO.BCM)
 
 handler = Handler(Buzzer(pin=20), LED(rot=22, gruen=23,blau=24), Servo(pin=17), Camera(0), PersonenHandler(),Gesichtserkennung(), Knopf(pin=5))
 handler.begin()
-print(handler.PersonenInBild())
+#print(handler.personen_in_bild())
 
-
-while True:
-    handler.loop()
+print("ready")
+try:
+    while True:
+        handler.loop()
+except Exception as e:
+    print('Error', str(e))
+    raise
