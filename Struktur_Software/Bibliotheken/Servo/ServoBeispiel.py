12be8c0b0012c987c5f809fe3f4ca84e084ff579
David Völlm <d-voellm@web.de>
2023-04-14 15:22:56 +0200

    kommentiert

--- Struktur_Software/Bibliotheken/Servo/ServoBeispiel.py
+++ Struktur_Software/Bibliotheken/Servo/ServoBeispiel.py
@@ -2,11 +2,11 @@ from Servo import Servo
 import RPi.GPIO as GPIO
 import time
 
-
 GPIO.setmode(GPIO.BCM)
-s = Servo(17)
-s.setAngle(90)
-time.sleep(2)
+
+s = Servo(17) # Servo Objekt erstellen
+s.setAngle(90) # Winkel setzen
+time.sleep(2) # warten
 s.setAngle(180)
 time.sleep(2)
 s.setAngle(0)
