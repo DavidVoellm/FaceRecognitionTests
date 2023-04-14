8b9121ba5a0f2a978eb7e0ec3d41e8b9ebbc199a
David Völlm <d-voellm@web.de>
2023-04-14 15:14:23 +0200

    kommentiert

--- Struktur_Software/Bibliotheken/PersonenHandler/Person.py
+++ Struktur_Software/Bibliotheken/PersonenHandler/Person.py
@@ -1,5 +1,4 @@
-class Person:
+class Person: # Klasse um Personen mit Name und Pfad zu ihrem Bild einfach gemeinsam übergeben zu können
     def __init__(self, name, image):
         self.name = name
         self.image = image
-        pass
