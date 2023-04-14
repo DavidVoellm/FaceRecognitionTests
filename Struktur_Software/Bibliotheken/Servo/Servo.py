ce380d31f59fbb5d6f66e8f1fecf08c3057b9c70
David Völlm <d-voellm@web.de>
2023-04-14 15:22:36 +0200

    kommentiert

--- Struktur_Software/Bibliotheken/Servo/Servo.py
+++ Struktur_Software/Bibliotheken/Servo/Servo.py
@@ -4,12 +4,11 @@ import time
 class Servo:
     def __init__(self, pin, frequency = 50):
         self.pin = pin
-        self.frequency = frequency
-        GPIO.setup(pin, GPIO.OUT)
-        self.p = GPIO.PWM(pin, 50)
+        GPIO.setup(pin, GPIO.OUT) # Pin als Output festlegen
+        self.p = GPIO.PWM(pin, frequency) # PWM Objekt erstellen um Servo zu steuern
         self.p.start(0)
-    def setAngle(self, angle:float):
-        pos = 2+(angle/18)
+    def setAngle(self, angle:float): # Servo in gewünschten Winkel ausrichten
+        pos = 2+(angle/18) # Durch Recherche und ausprobieren herausgefunden: dadurch dreht sich der Servo genau auf den gewünschten Winkel
         self.p.ChangeDutyCycle(pos)
         time.sleep(0.5)
         self.p.ChangeDutyCycle(0)
