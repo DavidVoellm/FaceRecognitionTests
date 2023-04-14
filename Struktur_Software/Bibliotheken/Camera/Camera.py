77ae62fc74e6d10283e1c447643d75119546dc1e
David Völlm <d-voellm@web.de>
2023-04-14 14:35:41 +0200

    kommentiert

--- Struktur_Software/Bibliotheken/Camera/Camera.py
+++ Struktur_Software/Bibliotheken/Camera/Camera.py
@@ -2,9 +2,9 @@ import cv2
 
 class Camera:
     def __init__(self, cam:int=0):
-        self.cam = cv2.VideoCapture(cam)
+        self.cam = cv2.VideoCapture(cam) # Mit open cv Kamera finden und nutzen
     def __del__(self):
         self.cam.release()
-    def get_frame(self): 
+    def get_frame(self): # "Foto" aufnehmen
         _, frame = self.cam.read()
         return frame
