b0dfe0f412696a0aef0e0a8e54a6e40543f90d37
David Völlm <d-voellm@web.de>
2023-04-14 14:34:09 +0200

    kommentiert

--- Struktur_Software/Bibliotheken/Buzzer/Buzzerbeispiel.py
+++ Struktur_Software/Bibliotheken/Buzzer/Buzzerbeispiel.py
@@ -1,15 +1,16 @@
-import sys
 import time
 import RPi.GPIO as GPIO
 from Buzzer import Buzzer
 
 GPIO.setmode(GPIO.BCM)
-buzzer = Buzzer(14)
+buzzer = Buzzer(14) # Buzzer Objekt erstellen
 time.sleep(0.5)
-buzzer.on()
+buzzer.on() # Ton starten
 
 time.sleep(1)
-buzzer.setFrequency(300)
+buzzer.setFrequency(300) # Frequenz ändern auf 300Hz
 time.sleep(1)
+
+# Beenden
+buzzer.stop()
 GPIO.cleanup()
-sys.exit()
