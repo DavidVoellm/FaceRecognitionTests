706dfe800fd42fd825bb2826965b2eb1312cb0ed
David Völlm <d-voellm@web.de>
2023-04-14 15:14:42 +0200

    kommentiert

--- Struktur_Software/Bibliotheken/PersonenHandler/PersonenHandler.py
+++ Struktur_Software/Bibliotheken/PersonenHandler/PersonenHandler.py
@@ -4,14 +4,13 @@ from .Person import Person
 class PersonenHandler:
     def __init__(self):
         self.personen = []
-    def find(self, dir): 
-        for file in os.listdir(dir):
-            if file.endswith(".jpg"):
-                name = file[:-4]
-                self.personen.append(Person(name, dir+"/"+file))
-    def get_personen(self):
+    def find(self, dir): # findet in gegebenem Pfad alle Bilder und erstellt aus der Benennung den dazugehörigen Namen
+        for file in os.listdir(dir):  # Geht durch jede Datei in gegebenem Ordner
+            if file.endswith(".jpg"): # Falls der Dateiname mit -jpg endet
+                name = file[:-4] # Name ist der Dateiname ohne die ".jpg"-Endung
+                self.personen.append(Person(name, dir+"/"+file)) # Person wird Liste hinzugefügt
         return self.personen
-    def sind_personen_berechtigt(self, names):
+    def sind_personen_berechtigt(self, names): # gibt True zurück wenn mindestens einer der Namen in der gespeicherten Namensliste vorkommt
         for person in self.personen:
             for name in names:
                 if person.name == name:
